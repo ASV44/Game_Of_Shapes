be291813831b5f71151b15fa26f1caee88608c48
Sasha <s.sasha44@yahoo.com>
2017-01-07 21:02:37 +0200

    Added HighScore class which manage scores for levels in each game mode

--- core/src/com/game/shapes/HighScores.java
+++ core/src/com/game/shapes/HighScores.java
@@ -14,29 +14,33 @@ import java.util.List;
 
 public class HighScores {
 
-    public List<Integer> countMoves = new ArrayList<Integer>();
-    public List<Integer> timeAttack = new ArrayList<Integer>();
-    public  List<Integer> timeChallenge = new ArrayList<Integer>();
+    public List<Integer> countMoves = null;
+    public List<Integer> timeAttack = null;
+    public  List<Integer> timeChallenge = null;
+
+    public String countMoves_dir = "Levels/countMoves.txt";
+    public String timeAttack_dir = "Levels/timeAttack.txt";
+    public String timeChallenge_dir = "Levels/timeChallenge.txt";
 
     public HighScores() {
-        try {
-            read_HighScore("Levels/countMoves.txt",countMoves);
-            read_HighScore("Levels/timeAttack.txt",timeAttack);
-            read_HighScore("Levels/timeChallenge.txt",timeChallenge);
-        }
-        catch(IOException exception) {
-            Gdx.app.log("Exception","",exception);
-        }
+        update_HighScores();
     }
 
-    public void read_HighScore(String ScoreList_name, List<Integer> scores) throws IOException {
-        FileHandle scoreList = Gdx.files.internal(ScoreList_name);
+    public List<Integer> read_HighScore(String ScoreList_name) throws IOException {
+        FileHandle scoreList;
+        List<Integer> scores = new ArrayList<Integer>();
+        if(Gdx.files.local(ScoreList_name).exists()) {
+            scoreList = Gdx.files.local(ScoreList_name);
+        } else {
+            scoreList = Gdx.files.internal(ScoreList_name);
+        }
         BufferedReader bufferedReader = new BufferedReader(scoreList.reader());
         int i = 0;
         String score;
 
         try {
             while((score = bufferedReader.readLine()) !=  null) {
+                Gdx.app.log("Score" + i,score);
                 scores.add(Integer.parseInt(score));
                 i++;
             }
@@ -44,5 +48,59 @@ public class HighScores {
         finally {
             bufferedReader.close();
         }
+
+        return scores;
+    }
+
+    public void update_HighScores() {
+        try {
+            countMoves = read_HighScore("Levels/countMoves.txt");
+            timeAttack = read_HighScore("Levels/timeAttack.txt");
+            timeChallenge = read_HighScore("Levels/timeChallenge.txt");
+        }
+        catch(IOException exception) {
+            Gdx.app.log("Exception","",exception);
+        }
+    }
+
+    public void append_HighScore(String directory, int score) {
+        FileHandle scoreList;
+
+        if(!Gdx.files.local(directory).exists()) {
+            scoreList = Gdx.files.local(directory);
+            scoreList.writeString(String.valueOf(score), true);
+        }
+        else {
+            scoreList = Gdx.files.local(directory);
+            scoreList.writeString("\n" + String.valueOf(score), true);
+        }
+    }
+
+    public void change_HighScore(String directory, int level, int score) {
+        FileHandle scoreList;
+        List<Integer> highScores = new ArrayList<Integer>();
+
+        if(!Gdx.files.local(directory).exists()) {
+            scoreList = Gdx.files.local(directory);
+            scoreList.writeString(String.valueOf(score), true);
+        }
+        else {
+            try { highScores = read_HighScore(directory); }
+            catch (IOException exception) { Gdx.app.log("Exception","",exception); }
+            if(score > highScores.get(level - 6)) { highScores.set(level - 6, score); }
+            scoreList = Gdx.files.local(directory);
+            for(int i = 0; i < highScores.size(); i++) {
+                if(i == 0 ) { scoreList.writeString(String.valueOf(highScores.get(i)),false);}
+                else { scoreList.writeString("\n" + String.valueOf(highScores.get(i)),true); }
+            }
+        }
+    }
+
+    public String getDir(String mode) {
+        if(mode.equals("Moves")) { return countMoves_dir; }
+        if(mode.equals("Time1")) { return timeChallenge_dir; }
+        if(mode.equals("Time")) { return  timeAttack_dir; }
+
+        return null;
     }
 }
